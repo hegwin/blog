20d19641c3bb3647bf7cfeeeb97bec10ba5688fc
hegwin <zwt315@163.com>
2013-10-21 17:01:50 +0800

    [#59131426] Add a rough method to degrade html header level

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  def degrade_head(html, descend_level=1)
+    (1..6-descend_level).to_a.reverse.each do |head|
+      html = html.gsub(/<h#{head}>/, "<h#{head+descend_level}>").gsub(/<\/h#{head}>/, "</h#{head+descend_level}>")
+    end
+    html
+  end
 end
--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -11,5 +11,31 @@ require 'spec_helper'
 #   end
 # end
 describe PostsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#degreade_head" do
+    it "should degrade the header level in html" do
+      html = %q[
+        <html>
+          <head><title></title></head>
+          <body>
+            <h1>Header 1</h1>
+            <h2>Header 2</h2>
+            <h3>Header 3</h3>
+            <h4>Header 4</h4>
+          </body>
+        </html>
+      ]
+      expected_html = %q[
+        <html>
+          <head><title></title></head>
+          <body>
+            <h3>Header 1</h3>
+            <h4>Header 2</h4>
+            <h5>Header 3</h5>
+            <h6>Header 4</h6>
+          </body>
+        </html>
+      ]
+      helper.degrade_head(html, 2).should eq expected_html
+    end
+  end
 end
