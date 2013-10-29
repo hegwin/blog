21688c9f27df812366ab0875cba9d7c499ab0d90
hegwin <zwt315@163.com>
2013-10-29 18:18:35 +0800

    Complete session controller and fix the specs for it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_filter :authorize
+
   def new
   end
 
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_url, notice: "Logged out." 
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -10,16 +10,25 @@ describe SessionsController do
   end
 
   describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+    let(:user) { create :user }
+
+    it "login successfully" do
+      post 'create', { name: user.name, password: user.password }
+      expect(session[:user_id]).to eq(user.id)
+      response.should redirect_to(admin_posts_url)
+    end
+
+    it "fails to login with incorrent info" do
+      post 'create', { name: user.name, password: "" }
+      response.should redirect_to(login_url)
     end
   end
 
   describe "GET 'destroy'" do
     it "returns http success" do
-      get 'destroy'
-      response.should be_success
+      delete 'destroy'
+      expect(session[:user_id]).to be_nil
+      response.should redirect_to(login_url)
     end
   end
 
