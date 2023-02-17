09c1dea8beb3c16fc04d6bb0340c30f5008dc706
Hegwin <zwt315@163.com>
2023-02-17 17:38:45 +0800

    [#184504325] Remove deprecated secret_token config

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '40ba68999da300df202751b1b6c720662051db8a3e48a73e7ee3c3b3a9d6860ca3f7e9710b0afe8b6b71079820767ee66dc7cf7641bff17af97863d4b29180c3'
